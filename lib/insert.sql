ce9398155c807c674ec4282df8f97bcddfa19e50
choim3 <choim3@spu.edu>
2020-11-29 21:35:36 -0800

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocolate", 2, "M", "Brown", "Shy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 3, "F", "BLACK", "Angry", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 6, "F", "Brown", "Happy", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 5, "M", "Brown", "Shy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 12, "F", "Brown", "Shy", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 3, "M", "Brown", "Angry", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 8, "F", "Brown", "Shy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 9, "M", "Brown", "Angry", 1);
